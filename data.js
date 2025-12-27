421d53229f63f9ffc3eaef8df9d291f4ba72aabd
Nithin Amaraboina <48430674+nithinpain@users.noreply.github.com>
2025-12-27 13:13:45 +0530

    Add questions array with quiz data

---
+++ data.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+    q: "INSPIRE Award MANAK is executed by which ministry?",
+    options: [
+      "Ministry of Education",
+      "Ministry of Science & Technology",
+      "NITI Aayog",
+      "DST & NIF"
+    ],
+    correct: 1
+  },
+  {
+    q: "Capital of India is?",
+    options: ["Mumbai", "Delhi", "Kolkata", "Chennai"],
+    correct: 1
+  }
+];
