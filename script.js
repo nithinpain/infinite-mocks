db282b4d0cf8f682e432042b02fb14030658ebca
Nithin Amaraboina <48430674+nithinpain@users.noreply.github.com>
2025-12-27 11:40:07 +0530

    Update exam title on DOMContentLoaded event
    Add event listener to update exam title on page load

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ document.getElementById("startBtn").addEventListener("click", function () {
 
   console.log("Starting mock for:", exam);
 
+  document.addEventListener("DOMContentLoaded", () => {
+  const params = new URLSearchParams(window.location.search);
+  const exam = params.get("exam");
+
+  if (exam && document.getElementById("examTitle")) {
+    document.getElementById("examTitle").innerText = `${exam} - Mock Test`;
+  }
+});
+
+
   // Later:
   // 1. Call backend API
   // 2. Load mock test page
